11c3faa68e53f1679df3c0db1451661836dc140f
Sathya Laufer <sathya@laufers.net>
2015-02-18 13:01:14 +0100

    Deb package

--- Scripts/CreateDebianPackage.sh
+++ Scripts/CreateDebianPackage.sh
@@ -17,20 +17,20 @@ wget https://github.com/Homegear/HomegearAddonLib/archive/$2.zip
 unzip $2.zip
 rm $2.zip
 version=$(head -n 1 HomegearAddonLib-$2/Version.h | cut -d " " -f3 | tr -d '"')
-sourcePath=libhomegear-addon-$version
+sourcePath=libhomegear-addon0-$version
 mv HomegearAddonLib-$2 $sourcePath
 rm -Rf $sourcePath/.* 1>/dev/null 2>&2
 rm -Rf $sourcePath/obj
 rm -Rf $sourcePath/bin
 rm -f $sourcePath/premake4*
-tar -zcpf libhomegear-addon_$version.orig.tar.gz $sourcePath
+tar -zcpf libhomegear-addon0_$version.orig.tar.gz $sourcePath
 cd $sourcePath
 dch -v $version-$1 -M
 debuild -us -uc
 cd ..
 rm -Rf $sourcePath
-rm libhomegear-addon_$version-?_*.build
-rm libhomegear-addon_$version-?_*.changes
-rm libhomegear-addon_$version-?.debian.tar.gz
-rm libhomegear-addon_$version-?.dsc
-rm libhomegear-addon_$version.orig.tar.gz
+rm libhomegear-addon0_$version-?_*.build
+rm libhomegear-addon0_$version-?_*.changes
+rm libhomegear-addon0_$version-?.debian.tar.gz
+rm libhomegear-addon0_$version-?.dsc
+rm libhomegear-addon0_$version.orig.tar.gz
